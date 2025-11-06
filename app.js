b48ce76238ed2fed04c056f90f03bb5db366d017
sudais <sudais@emil.com>
2025-11-07 01:50:10 +0330

    hk

---
+++ app.js
@@ -0,0 +1,9 @@
+const Fomr = document.querySelector('#Search-form')
+console.log(Fomr)
+const box = document.querySelector('#Search-box')
+console.log(box)
+const Result = document.querySelector('#search-Result')
+console.log(Result)
+
+const Load = document.querySelector('#Showemore')
+console.log(Load)
